ac018ad3134bc642b5ab0d3a253f3a9e6a6b696a
marcosjavier1997 <33172949+marcosjavier1997@users.noreply.github.com>
2023-10-24 20:42:10 -0300

    v1.6 +admin

--- Laboratorio/apps/persona/models.py
+++ Laboratorio/apps/persona/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Persona(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
     nombre = models.CharField(max_length=200)
     apellido = models.CharField(max_length=200)
     dni = models.CharField(max_length=8, unique=True)
--- Laboratorio/apps/trabajoFinalApp/admin.py
+++ Laboratorio/apps/trabajoFinalApp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.persona.models import Docente,Alumno,Asesor
+
+
+@admin.register(Docente)
+class DocenteAdmin(admin.ModelAdmin):
+    list_display = ("id","nombre","apellido","dni","cuil")
+
+@admin.register(Alumno)
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ("id","nombre","apellido","dni","mu")
+
+@admin.register(Asesor)
+class AsesorAdmin(admin.ModelAdmin):
+    list_display = ("id","nombre","apellido","dni")
